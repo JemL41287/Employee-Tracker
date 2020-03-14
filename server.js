6540d3473a2c5115bc83cb71efe6b3d4a313b54a
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-03-14 16:59:53 -0600

    added function to view all employees

--- server.js
+++ server.js
@@ -10,17 +10,35 @@ var connection = mysql.createConnection({
   database: "employee_trackerDB"
 });
 
+function updateServer() {
+  connection.query("SELECT * from role", function (err, res) {
+    allroles = res.map(role => ({ name: role.title, value: role.id }));
+  });
+
+  connection.query("SELECT * from department", function (err, res) {
+    alldepartments = res.map(dept => ({ name: dept.title, value: dept.id }));
+  });
+
+  connection.query("SELECT * from employee", function (err, res) {
+    allemployees = res.map(employee => ({ 
+      name: `${employee.first_name} ${employee.last_name}`,
+      value: employee.id
+   }));
+  });
+}
+
 connection.connect(function (err) {
   if (err) throw err;
   console.log("\nWelcome to the Employee Management System\n");
   startEmployeeManager();
+  updateServer();
 });
 
 function startEmployeeManager() {
   inquirer
     .prompt({
       name: "action",
-      list: "rawlist",
+      type: "rawlist",
       message: "What would you like to do?",
       choices: [
         "View All Employees",
@@ -69,3 +87,13 @@ function startEmployeeManager() {
       }
     });
 }
+
+function viewAllEmployees() {
+  var query = "SELECT employee.id, first_name AS firstname, last_name AS lastname, title AS role, name AS department, salary AS salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id;";
+  connection.query(query, function(err, res) {
+    if (err) throw (err);
+    console.table(res);
+    startEmployeeManager();
+  });
+}
+
